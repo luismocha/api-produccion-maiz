8797429037d880024bcc5d70f6b207c55b34cb1b
Jhonny-Morocho <luismocha21@gmail.com>
2022-12-07 15:49:57 -0500

    activando los cors

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -50,7 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
 ROOT_URLCONF = 'proyecto.urls'
 
 TEMPLATES = [
