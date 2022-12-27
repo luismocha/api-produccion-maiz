e91d93a75b5d0227f1ab554fd5087508efe75aab
Luis-Mocha <luismocha21@gmail.com>
2022-12-26 23:18:29 -0500

    corregido aactulizar usuario

--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -48,30 +48,12 @@ class UserSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
     def save(self):
-        #import pdb; pdb.set_trace()
-        try:
-            password = self.validated_data['password']
-            password2 = self.validated_data['password2']
-            if User.objects.filter(username=self.validated_data['username']).exists():
-                print("-----------ENCONTRADO USUARIO")
-                raise serializers.ValidationError({'error':'El nombre de usuario ya existe'})
-            else:
-                print("----------NO -ENCONTRADO USUARIO")
-            if password != password2:
-                raise serializers.ValidationError({'error':'El password de confirmación no coincide'})
-            
-            if User.objects.filter(email=self.validated_data['email']).exists():
-                raise serializers.ValidationError({'error':'El correo del usuario ya existe'})
-
-
-            ## TOODO OK
-            account = User(email=self.validated_data['email'],username=self.validated_data['username'])
-            account.set_password(password)
-            account.is_staff=self.validated_data['is_staff']
-            account.save()
-            return account
-        except Exception as e:
-            raise serializers.ValidationError({'error':'ERROR','message':str(e)})
+        account = User(email=self.validated_data['email'],username=self.validated_data['username'])
+        password = self.validated_data['password']
+        account.set_password(password)
+        account.is_staff=self.validated_data['is_staff']
+        account.save()
+        return account
 
     def update(self, instance, validated_data):
         instance.username=validated_data.get('username',instance.username)
