750f613dc097e99eace8878160cce28b264f72a9
Jhonny-Morocho <31082966+luismocha@users.noreply.github.com>
2023-01-07 15:45:05 -0500

    crenado migracion con posgres

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%rv8vd04(z_s*9h@zn141%ma5-$_ur_3u!hxasr9xmngiso2wx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
@@ -80,13 +80,25 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}  """
 
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'postgres',
+        'USER' : 'postgres',
+        'PASSWORD' : 'root',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -112,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
@@ -141,3 +153,4 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ] 
 }
+
